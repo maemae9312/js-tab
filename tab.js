fb18817bec7a9ea257906f7b4a96dbe940fc9f4d
maemae9312 <maeda.hyo@kikitori.jp>
2023-08-23 21:42:32 +0900

    javascript_implementation

--- tab.js
+++ tab.js
@@ -15,8 +15,40 @@
     // 初期化
     // JavaScriptで最初に実行させたい命令の時に、initをよく使う
     const init = () => {
-
+        $content[0].style.display = 'block';
     };
+    init();
+
+    // クリックしたら起こるイベント
+    const handleClick = (e) => {
+        e.preventDefault();
+
+        // クリックされたnavとそのdataを取得
+        const $this = e.target;
+        const targetVal = $this.dataset.nav;
+
+        //対象外のnav,content全てリセットする
+        let index = 0;
+        while(index < $nav.length){
+            $content[index].style.display = 'none';
+            $nav[index].classList.remove('is-active');
+            index++;
+        }
+
+        // 対象のコンテンツをアクティブ化する
+        $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
+        $nav[targetVal].classList.add('is-active');
+    };
+
+    // 全nav要素に対して関数を適応•発火
+    let index = 0;
+    while(index < $nav.length){
+        $nav[index].addEventListener('click', (e) => handleClick(e));
+        index++
+    }
+
+    
+
 })();
 
 // メモ
